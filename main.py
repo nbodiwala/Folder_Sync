d17742e2b18f4de8cbecda8676e1239ebda067d5
Nirav Bodiwala <nbodiwala@gmail.com>
2024-01-23 11:13:34 -0600

    removed local dependencies

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 import csv
 from subprocess import call
-from config import csv_path
+from config import csv_path, recipient_list_path
+import win32com.client
+
+
+def generate_recipient_list():
+    txt = open(recipient_list_path, 'r')
+    lines = txt.readlines()
+
+    recipients = ''
+    for line in lines:
+            line = line.rstrip()
+            recipients += line + '; '
+
+    return recipients
+
 
 with open(csv_path, mode='r') as csv_file:
     csv_reader = csv.DictReader(csv_file)
@@ -9,3 +23,11 @@ with open(csv_path, mode='r') as csv_file:
         destination = line['Destination']
 
         call(['robocopy', source, destination, '/MIR', '/S', '/XD', '/XO', '*_archive'])
+
+# Create and send Folder Sync Status Email
+outlook = win32com.client.Dispatch('outlook.application')
+mail = outlook.CreateItem(0)
+mail.To = generate_recipient_list()
+mail.Subject = 'Folder Sync Process Has Successfully Run'
+
+mail.Send()
